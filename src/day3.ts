851ca33df60e03406bcd2ee1674b73a745db97fb
Tuan Tran <tuanmt@fb.com>
2019-01-03 00:19:30 -0800

    day 3. TODO: cleanup code

---
+++ src/day3.ts
@@ -0,0 +1,37 @@
+import * as fs from "fs";
+
+const inputPath = "data/day3.txt";
+const lines = fs
+  .readFileSync(inputPath)
+  .toString()
+  .split("\n");
+
+const claims = lines.map(line => (line.match(/\d+/g) || []).map(Number));
+const claimInverseMap = new Map<string, number[]>();
+const overlapClaimsSet = new Set<number>();
+let result = 0;
+
+for (const claim of claims) {
+  const [id, originX, originY, width, height] = claim;
+  overlapClaimsSet.add(id);
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      const key = [originX + x, originY + y].toString();
+      const claimList = claimInverseMap.get(key) || [];
+      if (claimList.length === 1) {
+        result++;
+      }
+      for (const prevId of claimList) {
+        overlapClaimsSet.delete(prevId);
+      }
+      if (claimList.length > 0) {
+        overlapClaimsSet.delete(id);
+      }
+      claimList.push(id);
+      claimInverseMap.set(key, claimList);
+    }
+  }
+}
+
+console.log(result);
+console.log(overlapClaimsSet);
