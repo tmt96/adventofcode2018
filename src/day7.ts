ffb8f60871d4d5d6133e696fd6542b5402e29f74
Tuan Tran <tuanmt@fb.com>
2019-01-19 20:13:58 -0800

    reorganize day 7

--- src/day7.ts
+++ src/day7.ts
@@ -6,51 +6,77 @@ const regex = /Step ([A-Z]) must be finished before step ([A-Z]) can begin./;
 
 const dependenciesMap = new Map<string, Set<string>>();
 const nextStepsMap = new Map<string, Set<string>>();
-const stepQueue: string[] = [];
-const alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-let result = "";
+const stepList = new Set<string>();
+// const alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
 
 // setup
-for (const step of alphabet) {
-  dependenciesMap.set(step, new Set());
-  nextStepsMap.set(step, new Set());
-}
-
 for (const line of lines) {
   const match = line.match(regex);
   if (match !== null) {
     const [_, prev, next] = match;
+
+    // fill the list & maps
+    stepList.add(prev);
+    stepList.add(next);
+    if (!dependenciesMap.has(next)) {
+      dependenciesMap.set(next, new Set());
+    }
+    if (!dependenciesMap.has(prev)) {
+      dependenciesMap.set(prev, new Set());
+    }
+    if (!nextStepsMap.has(prev)) {
+      nextStepsMap.set(prev, new Set());
+    }
+    if (!nextStepsMap.has(next)) {
+      nextStepsMap.set(next, new Set());
+    }
+
     dependenciesMap.get(next)!.add(prev);
     nextStepsMap.get(prev)!.add(next);
   }
 }
 
-// part 1
-for (const [key, val] of dependenciesMap) {
-  if (val.size === 0) {
-    stepQueue.push(key);
-  }
-}
-
-function nextStep(): string | null {
-  stepQueue.sort().reverse();
-  const step = stepQueue.pop();
-  if (step === undefined) {
-    return null;
-  }
-  for (const followingStep of nextStepsMap.get(step) || []) {
-    dependenciesMap.get(followingStep)!.delete(step);
-    if (dependenciesMap.get(followingStep)!.size === 0) {
+function populateQueues(
+  stepQueue: string[],
+  completedStep: string,
+  result: string
+): void {
+  for (const followingStep of nextStepsMap.get(completedStep) || []) {
+    const dependencies = dependenciesMap.get(followingStep)!;
+    if (Array.from(dependencies).every(c => result.includes(c))) {
       stepQueue.push(followingStep);
     }
   }
-  nextStepsMap.delete(step);
-
-  return step;
 }
 
-while (nextStepsMap.size > 0) {
-  result += nextStep();
+function nextStep(stepQueue: string[]): string | undefined {
+  stepQueue.sort().reverse();
+  return stepQueue.pop();
 }
 
-console.log(result);
+function part1(): string {
+  let result = "";
+  const stepQueue = [];
+  for (const [key, val] of dependenciesMap) {
+    if (val.size === 0) {
+      stepQueue.push(key);
+    }
+  }
+
+  for (let i = 0; i < stepList.size; i++) {
+    const step = nextStep(stepQueue);
+    if (step !== undefined) {
+      result += step;
+      populateQueues(stepQueue, step, result);
+    }
+  }
+  return result;
+}
+
+function part2(): number {
+  let result = 0;
+  return result;
+}
+
+console.log(part1());
+console.log(part2());
